3dbb71ee93f9aff1cdcdd722dad4b9eb2b8b615c
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-29 12:05:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Counter } from "./Counter/Counter";
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+//
---
+++ src/Context/AppContext.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export const AppContext = React.createContext();
+
+const initState = {
+  counter: 0
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment": {
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    }
+    case "decrement": {
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    }
+    case "reset": {
+      return {
+        ...state,
+        counter: (state.counter = 0)
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+function AppContextProvider({ children }) {
+  const [state, dispatch] = React.useReducer(reducer, initState);
+
+  const value = { state, dispatch };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+}
+
+export { AppContextProvider };
